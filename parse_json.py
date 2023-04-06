44f2ff5edcaef2184829ef3ca654aa29323412ee
Janete Neto <janeteneto26@gmail.com>
2023-04-06 13:58:01 +0100

    added json2yaml

--- parse_json.py
+++ parse_json.py
@@ -1,5 +1,21 @@
 import json
+import os
 
-json = json.loads(open("C:/Users/user/pythonProject/why_python/eg.json").read())
+# json = json.loads(open("C:/Users/user/pythonProject/why_python/eg.json").read())
+# value = json['name']
+# print(value)
+# script to create absolute path variable
+
+script_dir = os.path.dirname(__file__)
+print("The script is located at:" + script_dir)
+script_absolute_path = os.path.join(script_dir, 'eg.json')
+print("The script path is:" + script_absolute_path)
+
+# script parse
+json = json.loads(open(script_absolute_path).read())
 value = json['name']
 print(value)
+
+# loop through json keys and values, print them out
+for key in json:
+    print(key + ': ' + json[key])
